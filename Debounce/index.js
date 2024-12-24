de7355e5d44df7ec3b719785ed1d97a8e97fe559
mahenv <mahendrauppari044@gmail.com>
2024-12-24 00:34:28 -0500

    adding debounce

---
+++ Debounce/index.js
@@ -0,0 +1,47 @@
+// "use strict";
+const API = "https://api.github.com/users/";
+
+async function fetchGitUser(name) {
+  const existingSpan = document.querySelector("span");
+
+  if (existingSpan) {
+    existingSpan.remove();
+  }
+
+  const data = await fetch(API + name);
+  const res = await data.json();
+  console.log(res);
+  const a = document.createElement("span");
+  a.textContent = JSON.stringify(res, null, 2);
+  document.body.appendChild(a);
+}
+
+function debounce(func, wait) {
+  let timer = null;
+
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, ...args);
+    }, wait);
+  };
+}
+
+function sayHello() {
+  console.log("My name is", this.name);
+}
+
+function getMyGitData() {
+  console.log(document.getElementById("name").value);
+
+  fetchGitUser(document.getElementById("name").value);
+}
+
+const debouncedFn = debounce(getMyGitData, 1000);
+
+// const obj = {
+//   name: "mahe",
+//   speak: debounce(sayHello),
+// };
+
+// obj.speak();
